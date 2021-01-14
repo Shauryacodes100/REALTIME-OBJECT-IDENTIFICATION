c8f9221beddf33dfead1fb304b095711631aae61
Shauryacodes100 <76858217+Shauryacodes100@users.noreply.github.com>
2021-01-14 07:19:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ function setup(){
     canvas=createCanvas(380, 380);
     canvas.center();
 }
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects"
+}
+
+function moedlLoaded(){
+    console.log("modelLoaded");
+    status=true;
+}v
